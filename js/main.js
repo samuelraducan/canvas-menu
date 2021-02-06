d32045f05379bbd584539734d5570d0f682f97df
Samuel Raducan <samuel.raducann@gmail.com>
2021-02-06 17:36:13 +0000

    Added accessibility escape button and focus.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,44 @@
 /** Global Variables */
 const button = document.querySelector('button');
 const body = document.body;
+const menu = document.querySelector('.nav');
+
+// =====================
+/** Functions */
+// =====================
+
+/**
+ * Checks whether the sidebar is open
+ * @param {*}
+ */
+const isOffCanvasMenuOpen = _ => {
+  return body.classList.contains('offsite-is-open');
+};
+
+/**
+ * Close sidebar
+ */
+const closeOffCanvasMenu = _ => {
+  body.classList.remove('offsite-is-open');
+  button.focus();
+};
+
+/**
+ * Open sidebar
+ */
+const openOffCanvasMenu = _ => {
+  body.classList.add('offsite-is-open');
+  menu.focus();
+};
 
 /** Event Listeners */
 button.addEventListener('click', event => {
-  body.classList.toggle('offsite-is-open');
+  isOffCanvasMenuOpen() ? closeOffCanvasMenu() : openOffCanvasMenu();
+});
+
+document.addEventListener('keydown', event => {
+  // Close the menu (Escape Key)
+  if (isOffCanvasMenuOpen() && event.key === 'Escape') {
+    closeOffCanvasMenu();
+  }
 });
